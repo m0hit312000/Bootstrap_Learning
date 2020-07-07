05c0e18aa1de627ac6f06152078646b7b1aea4ea
Mohit <mm8085574989@gmail.com>
2020-07-07 19:09:45 +0530

    NPM part 1

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval:2000});
+    $("#carouselButton").click(function()
+    {
+     if ($("#carouselButton").children("span").hasClass('fa-pause')) 
+     {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+     }
+     else if ($("#carouselButton").children("span").hasClass('fa-play'))
+     {
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+     }
+    });
+    $("#modalButton").click(function(){
+         $("#loginModal").modal("show");                                                                 
+    });
+    $("#closeButton").click(function(){
+         $("#loginModal").modal("hide");                                                                 
+    }); 
+    $("#closeButton2").click(function(){
+         $("#loginModal").modal("hide");                                                                 
+    });
+    $("#resBut").click(function(){
+         $("#resTable").modal("show");                                                                 
+    }); 
+    $("#closeButton3").click(function(){
+         $("#resTable").modal("hide");                                                                 
+    });
+    $("#closeButton4").click(function(){
+         $("#resTable").modal("hide");                                                                 
+    });
+ });
