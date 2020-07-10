26938d2ae36c616562432861f4a7429319fb0009
Mohit <mm8085574989@gmail.com>
2020-07-10 17:44:45 +0530

    Grunt part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,11 @@
 module.exports = function(grunt) {
    
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt,{
+        useminPrepare: 'grunt-usemin'
+    });
     
-    grunt.initConfig({
+grunt.initConfig({
      sass:{
         dist: {
             files: {
@@ -33,9 +35,132 @@ module.exports = function(grunt) {
                  }
              }
          }
-     }
+     },
+     copy: {
+        html: {
+            files: [
+            {
+                //for html
+                expand: true,
+                dot: true,
+                cwd: './',
+                src: ['*.html'],
+                dest: 'dist'
+            }]                
+        },
+        fonts: {
+            files: [
+            {
+                //for font-awesome
+                expand: true,
+                dot: true,
+                cwd: 'node_modules/font-awesome',
+                src: ['fonts/*.*'],
+                dest: 'dist'
+            }]
+        }
+    },
 
-    });
-    grunt.registerTask('css',['sass']);
-    grunt.registerTask('default',['browserSync','watch']);
+    clean: {
+        build: {
+            src: [ 'dist/']
+        }
+      },
+        
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,                  // Enable dynamic expansion
+                cwd: './',                   // Src matches are relative to this path
+                src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
+                dest: 'dist/'                  // Destination path prefix
+            }]
+        }
+     },
+     useminPrepare: {
+         foo: {
+             dest: 'dist',
+             src: ['contactus.html', 'aboutus.html', 'index.html']
+         },
+         options: {
+             flow: {
+                 steps: {
+                     css: ['cssmin'],
+                     js: ['uglify']
+                 },
+                 post: {
+                      css: [{
+                          name: 'cssmin',
+                          createConfig: function (context,block)
+                          {
+                              var generated = context.options.generated;
+                              generated.options = {
+                                    keepSpecialComments: 0,rebase: false
+                              };
+                          }
+                      }]  
+                 }
+             }
+         }
+     },
+     concat: {
+         options: {
+             seperator: ';'
+         },
+         dist: {}
+     },
+     uglify: {
+          dist: {}
+     },
+     cssmin: {
+         dist: {}
+     },
+     filerev: {
+         options: {
+             encoding: 'utf8',
+             algorithm: 'md5',
+             length: 20
+         },
+         release: {
+             files: [{
+                 src: [
+                     'dist/js/*.js',
+                     'dist/css/*.css',
+
+                 ]
+             }]
+         } 
+     },
+     usemin: {
+         html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+         options: {
+             assetsDirs: ['dist', 'dist/css', 'dist/js']
+         }
+     },
+     htmlmin: {
+         dist: {
+             options: {
+                 collapseWhiteSpace: true
+             },
+             files: {
+                 'dist/index.html': 'dist/index.html',
+                 'dist/contactus.html': 'dist/contactus.html',
+                 'dist/aboutus.html': 'dist/aboutus.html'
+             }
+         }
+     }
+});
+grunt.registerTask('css',['sass']);
+grunt.registerTask('default',['browserSync','watch']);
+grunt.registerTask('build',[
+    'clean',
+    'copy',
+    'imagemin',
+    'useminPrepare',
+    'cssmin',
+    'uglify',
+    'filerev',
+    'usemin',
+    'htmlmin'
+  ]);
 }
